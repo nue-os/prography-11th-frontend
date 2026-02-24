66a68e79c2ba92935f2e7821285d927b7e50df71
nue-os <kimsoeun0108@gmail.com>
2026-02-25 03:07:03 +0900

    feat: 관리자 페이지 레이아웃 적용

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import UserHistory from '../pages/UserHistory';
 import UserModify from '../pages/UserModify';
+import AdminLayout from '../layout/AdminLayout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <UserHistory />,
-  },
-  {
-    path: '/modify',
-    element: <UserModify />,
+    element: <AdminLayout />,
+    children: [
+      {
+        path: '/admin/users',
+        element: <UserHistory />,
+      },
+      {
+        path: '/admin/user/modify',
+        element: <UserModify />,
+      },
+    ],
   },
 ]);
