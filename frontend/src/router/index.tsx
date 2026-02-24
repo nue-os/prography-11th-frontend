146a623d9776d953f51a240fd88ef3382ebc21f3
nue-os <kimsoeun0108@gmail.com>
2026-02-25 02:20:25 +0900

    feat: 회원 수정 라우터 등록

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import UserHistory from '../pages/UserHistory';
+import UserModify from '../pages/UserModify';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <UserHistory />,
+  },
+  {
+    path: '/modify',
+    element: <UserModify />,
   },
 ]);
