6e73f97eb584ca6dd0fe4080381c35f0f8d1b60b
nue-os <kimsoeun0108@gmail.com>
2026-03-08 21:34:16 +0900

    feat: 회원 등록 API 구현

--- frontend/src/apis/user.ts
+++ frontend/src/apis/user.ts
@@ -1,9 +1,14 @@
 import admin from '.';
-import { GetUsersParams } from '../types/admin';
+import { GetUsersParams, UserForm } from '../types/admin';
 
 export const getUsers = async (params: GetUsersParams) => {
   const response = await admin.get('/members', {
     params,
   });
+  return response.data;
+};
+
+export const postUser = async (data: UserForm) => {
+  const response = await admin.post('/members', data);
   return response.data;
 };
